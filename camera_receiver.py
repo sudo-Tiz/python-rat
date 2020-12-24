890360180d75ed20f7d5f0bf1e1359af73e15ec7
sudoTiz <Le_Tiz@laposte.net>
2020-12-24 14:34:47 +0100

    camera0.1

---
+++ camera_receiver.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import cv2
+import numpy as np
+import socket
+import struct
+
+MAX_DGRAM = 2**16
+
+def dump_buffer(s):
+    """ Emptying buffer frame """
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        print(seg[0])
+        if struct.unpack("B", seg[0:1])[0] == 1:
+            print("finish emptying buffer")
+            break
+
+def main(host='127.0.0.1', port=5000):
+    
+    # Set up socket
+    s = socket.socket()
+    s.connect((host, port))
+    dat = b''
+    dump_buffer(s)
+
+    while True:
+        seg, addr = s.recvfrom(MAX_DGRAM)
+        if struct.unpack("B", seg[0:1])[0] > 1:
+            dat += seg[1:]
+        else:
+            dat += seg[1:]
+            img = cv2.imdecode(np.fromstring(dat, dtype=np.uint8), 1)
+            cv2.imshow('frame', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            dat = b''
+
+    # cap.release()
+    cv2.destroyAllWindows()
+    s.close()
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+    
+    cli_args = argparse.ArgumentParser()
+    cli_args.add_argument('--host', default='127.0.0.1', type=str)
+    cli_args.add_argument('--port', default=5000, type=int)
+    options = cli_args.parse_args(sys.argv[1:])
+    
+    main(host=options.host, port=options.port)
---
+++ camera_sender.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import cv2
+import numpy as np
+import socket
+import struct
+import math
+import threading
+MAX_DGRAM = 2**16
+MAX_IMAGE_DGRAM = MAX_DGRAM - 64 # extract 64 bytes in case UDP frame overflown
+
+
+def camera_stream(conn):
+
+    cap = cv2.VideoCapture(0)
+    while (cap.isOpened()):
+        _, frame = cap.read()
+        compress_img = cv2.imencode('.jpg', frame)[1]
+        dat = compress_img.tostring()
+        size = len(dat)
+        count = math.ceil(size/(MAX_IMAGE_DGRAM))
+        array_pos_start = 0
+        while count:
+            array_pos_end = min(size, array_pos_start + MAX_IMAGE_DGRAM)
+            conn.send(struct.pack("B", count)  +dat[array_pos_start:array_pos_end])
+            array_pos_start = array_pos_end
+            count -= 1
+    cap.release()
+    cv2.destroyAllWindows()
+    conn.close()
+
+def main(host='0.0.0.0', port=5000):
+    with socket.socket() as sock:
+        sock.bind((host, port))
+        sock.listen(5)
+        print('Server started.')
+
+        while 'connected':
+            conn, addr = sock.accept()
+            print('Client connected IP:', addr)
+            thread = threading.Thread(target=camera_stream, args=(conn,))
+            thread.start()
+
+
+if __name__ == '__main__':
+    import argparse
+    import sys
+    
+    cli_args = argparse.ArgumentParser()
+    cli_args.add_argument('--port', default=5000, type=int)
+    options = cli_args.parse_args(sys.argv[1:])
+    
+    main(port=options.port)
